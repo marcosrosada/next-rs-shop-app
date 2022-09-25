9d14721b916c387f0dcad1070f088cbfc0a7d476
Marcos Rosada <Marcos.Rosada@bjss.com>
2022-09-25 21:13:09 +0100

    Adding Logo and App styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import type { AppProps } from 'next/app';
 import { globalStyle } from '../styles/global';
 
+import logoImg from '../assets/logo.svg';
+import { Container, Header } from '../styles/pages/app';
+
 globalStyle();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  );
 }
