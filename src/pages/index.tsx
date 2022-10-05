64d0f7fc984d708b37bbed9e80936a4520157a26
Marcos Rosada <Marcos.Rosada@bjss.com>
2022-10-05 20:39:38 +0100

    Add list

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,25 @@ import type { NextPage } from 'next';
 import Image from 'next/image';
 import { HomeContainer, Product } from '../styles/pages/home';
 
+import { useKeenSlider } from 'keen-slider/react';
+
 import camiseta1 from '../assets/camisetas/1.png';
 import camiseta2 from '../assets/camisetas/2.png';
 import camiseta3 from '../assets/camisetas/3.png';
 
+import 'keen-slider/keen-slider.min.css';
+
 const Home: NextPage = () => {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  });
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={camiseta1} width={520} height={480} alt="" />
 
         <footer>
@@ -18,8 +29,26 @@ const Home: NextPage = () => {
         </footer>
       </Product>
 
-      <Product>
+      <Product className="keen-slider__slide">
         <Image src={camiseta2} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} width={520} height={480} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={camiseta3} width={520} height={480} alt="" />
 
         <footer>
           <strong>Camiseta X</strong>
