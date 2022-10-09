f4fdccd2081552ca8c852ad8b9f9619a5836473d
Marcos Rosada <Marcos.Rosada@bjss.com>
2022-10-09 18:37:59 +0100

    Change Get Static Props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,10 @@
-import type { GetServerSideProps } from 'next';
+import type { GetStaticProps } from 'next';
 import Image from 'next/image';
 
 import { useKeenSlider } from 'keen-slider/react';
 
 import { stripe } from '../lib/stripe';
 import { HomeContainer, Product } from '../styles/pages/home';
-
-import camiseta1 from '../assets/camisetas/1.png';
-import camiseta2 from '../assets/camisetas/2.png';
-import camiseta3 from '../assets/camisetas/3.png';
 
 import 'keen-slider/keen-slider.min.css';
 import Stripe from 'stripe';
@@ -47,7 +43,7 @@ export default function Home({ products }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price'],
   });
@@ -67,5 +63,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       products,
     },
+    revalidate: 60 * 60 * 2, // 2 hours
   };
 };
